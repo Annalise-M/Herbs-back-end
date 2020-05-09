0f7859cbe3b3050e1f173f8352332748d7f5510b
Annalise Murphy <nali247@gmail.com>
2020-05-08 17:50:46 -0700

    server.js linking from front-end

--- server.js
+++ server.js
@@ -5,18 +5,26 @@ const client = require('./lib/client');
 // Initiate database connection
 client.connect();
 
-const app = require('./lib/app');
+const app = require('./lib/client');
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/herbs', async(req, res) => {
+//All Herbs
+app.get('./Herbs', async(req, res) => {
   const data = await client.query('SELECT * from herbs');
 
   res.json(data.rows);
 });
 
+//just ONE Herb
+app.get('./Herbs/:name', async(req, res) => {
+  const name = req.params.name;
+  const data = await client.query('SELECT * from herbs where name=$1'[name]);
+
+  res.json(data.rows);
+});
+
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
 });
 
